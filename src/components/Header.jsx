34a944d83b4067cc7d75ac036ec8ebde514ebc99
Yusuf <yusufonarandx@hotmail.com>
2024-05-11 23:41:41 +0300

    add download

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Button } from './ui/button'
+import React from "react";
+import { Button } from "./ui/button";
 import { FaDownload } from "react-icons/fa6";
 
-function Header() {
+function Header({DownloadIcon}) {
   return (
-    <div className='p-4 shadow-sm border flex justify-between items-center'>
-        <img src="/vite.svg" alt="" />
-        <Button className="flex gap-2 items-center text-white"> <FaDownload className='h-4 w-4'/> Download </Button>
+    <div className="p-4 shadow-sm border flex justify-between items-center">
+      <img src="/vite.svg" alt="" />
+      <Button className="flex gap-2 items-center text-white" onClick={()=>DownloadIcon(Date.now())}>
+        <FaDownload className="h-4 w-4" />
+        Download
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
